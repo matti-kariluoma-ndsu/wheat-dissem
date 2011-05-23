1157a77ff479f84e2f16741e1a72fe0029e96db9
matti <matti@porthos-debian.(none)>
2011-05-23 13:50:46 -0500

    Added some server heavy-lifting for noscript clients. Changes remain to be reflected in the html templates.
    -Matti Kariluoma May 2011

--- wheat_website/wheat_data/models.py
+++ wheat_website/wheat_data/models.py
@@ -3,103 +3,103 @@ from django.forms import ModelForm
 
 # Create your models here.
 class Disease(models.Model):
-  name = models.CharField(max_length=200)
+	name = models.CharField(max_length=200)
 
-  def __unicode__(self):
-    return self.name
+	def __unicode__(self):
+		return self.name
 
 class Variety(models.Model):
-  name            = models.CharField(max_length=200)
-  description_url = models.CharField(max_length=200, blank=True, null=True)
-  picture_url     = models.CharField(max_length=200, blank=True, null=True)
-  agent_origin    = models.CharField(max_length=200, blank=True, null=True)
-  year_released   = models.CharField(max_length=200, blank=True, null=True)
-  straw_length    = models.CharField(max_length=200, blank=True, null=True)
-  maturity        = models.CharField(max_length=200, blank=True, null=True)
-  grain_color     = models.CharField(max_length=200, blank=True, null=True)
-  seed_color      = models.CharField(max_length=200, blank=True, null=True)
-  beard           = models.CharField(max_length=200, blank=True, null=True)
-  wilt            = models.CharField(max_length=200, blank=True, null=True)
-  diseases        = models.ManyToManyField(Disease, through='Disease_Entry', blank=True, null=True)
-  
-  def __unicode__(self):
-    return self.name
-  
-  class Meta:
-    ordering = ["-name"]
+	name						= models.CharField(max_length=200)
+	description_url = models.CharField(max_length=200, blank=True, null=True)
+	picture_url		 = models.CharField(max_length=200, blank=True, null=True)
+	agent_origin		= models.CharField(max_length=200, blank=True, null=True)
+	year_released	 = models.CharField(max_length=200, blank=True, null=True)
+	straw_length		= models.CharField(max_length=200, blank=True, null=True)
+	maturity				= models.CharField(max_length=200, blank=True, null=True)
+	grain_color		 = models.CharField(max_length=200, blank=True, null=True)
+	seed_color			= models.CharField(max_length=200, blank=True, null=True)
+	beard					 = models.CharField(max_length=200, blank=True, null=True)
+	wilt						= models.CharField(max_length=200, blank=True, null=True)
+	diseases				= models.ManyToManyField(Disease, through='Disease_Entry', blank=True, null=True)
+	
+	def __unicode__(self):
+		return self.name
+	
+	class Meta:
+		ordering = ["-name"]
 
 class Disease_Entry(models.Model):
-  disease        = models.ForeignKey(Disease)
-  variety        = models.ForeignKey(Variety)
-  susceptibility = models.DecimalField(decimal_places=5, max_digits=8)
-  
-  def __unicode__(self):
-    return str(self.variety) + " has a " + str(self.susceptibility) 
-    + "% susceptibility to " + str(self.disease)
+	disease				= models.ForeignKey(Disease)
+	variety				= models.ForeignKey(Variety)
+	susceptibility = models.DecimalField(decimal_places=5, max_digits=8)
+	
+	def __unicode__(self):
+		return str(self.variety) + " has a " + str(self.susceptibility) 
+		+ "% susceptibility to " + str(self.disease)
 
 class Zipcode(models.Model):
-  zipcode          = models.PositiveIntegerField()
-  city             = models.CharField(max_length=200)
-  state            = models.CharField(max_length=2)
-  latitude         = models.DecimalField(decimal_places=10, max_digits=13)
-  longitude        = models.DecimalField(decimal_places=10, max_digits=13)
-  timezone         = models.SmallIntegerField()
-  daylight_savings = models.SmallIntegerField()
-  
-  def __unicode__(self):
-    return str(self.zipcode).zfill(5) + ": " + self.city + ", " + self.state
+	zipcode					= models.PositiveIntegerField()
+	city						 = models.CharField(max_length=200)
+	state						= models.CharField(max_length=2)
+	latitude				 = models.DecimalField(decimal_places=10, max_digits=13)
+	longitude				= models.DecimalField(decimal_places=10, max_digits=13)
+	timezone				 = models.SmallIntegerField()
+	daylight_savings = models.SmallIntegerField()
+	
+	def __unicode__(self):
+		return str(self.zipcode).zfill(5) + ": " + self.city + ", " + self.state
 
 class Location(models.Model):
-  name      = models.CharField(max_length=200)
-  zipcode   = models.ForeignKey(Zipcode)
-  latitude  = models.DecimalField(decimal_places=10, max_digits=13, blank=True, null=True)
-  longitude = models.DecimalField(decimal_places=10, max_digits=13, blank=True, null=True)
+	name			= models.CharField(max_length=200)
+	zipcode	 = models.ForeignKey(Zipcode)
+	latitude	= models.DecimalField(decimal_places=10, max_digits=13, blank=True, null=True)
+	longitude = models.DecimalField(decimal_places=10, max_digits=13, blank=True, null=True)
 
-  
-  def __unicode__(self):
-    return self.name
+	
+	def __unicode__(self):
+		return self.name
 
 class Date(models.Model):
-  date  = models.DateField()
-  
-  def __unicode__(self):
-    return str(self.date)
-  
+	date	= models.DateField()
+	
+	def __unicode__(self):
+		return str(self.date)
+	
 class Trial_Entry(models.Model):
-  bushels_acre         = models.DecimalField(decimal_places=5, max_digits=10)
-  protein_percent      = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  test_weight          = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
-  kernel_weight        = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
-  plant_height         = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
-  days_to_head         = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  lodging_factor       = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  jday_of_head         = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  winter_survival_rate = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  shatter              = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  seeds_per_round      = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  canopy_density       = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  canopy_height        = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
-  days_to_flower       = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  seed_oil_percent     = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
-  planting_method_tags = models.CharField(max_length=200, blank=True, null=True)
-  seeding_rate         = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
-  previous_crop        = models.CharField(max_length=200, blank=True, null=True)
-  moisture_basis       = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
-  plant_date           = models.ForeignKey(Date, related_name='plant_date', blank=True, null=True)
-  harvest_date         = models.ForeignKey(Date, related_name='harvest_date')
-  location             = models.ForeignKey(Location)
-  variety              = models.ForeignKey(Variety)
+	bushels_acre				 = models.DecimalField(decimal_places=5, max_digits=10)
+	protein_percent			= models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	test_weight					= models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
+	kernel_weight				= models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
+	plant_height				 = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
+	days_to_head				 = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	lodging_factor			 = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	jday_of_head				 = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	winter_survival_rate = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	shatter							= models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	seeds_per_round			= models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	canopy_density			 = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	canopy_height				= models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
+	days_to_flower			 = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	seed_oil_percent		 = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
+	planting_method_tags = models.CharField(max_length=200, blank=True, null=True)
+	seeding_rate				 = models.DecimalField(decimal_places=5, max_digits=8, blank=True, null=True)
+	previous_crop				= models.CharField(max_length=200, blank=True, null=True)
+	moisture_basis			 = models.DecimalField(decimal_places=5, max_digits=10, blank=True, null=True)
+	plant_date					 = models.ForeignKey(Date, related_name='plant_date', blank=True, null=True)
+	harvest_date				 = models.ForeignKey(Date, related_name='harvest_date')
+	location						 = models.ForeignKey(Location)
+	variety							= models.ForeignKey(Variety)
 
-  def __unicode__(self):
-    return str(self.variety)+" at "+str(self.location)+", "+str(self.harvest_date.date.year)
+	def __unicode__(self):
+		return str(self.variety)+" at "+str(self.location)+", "+str(self.harvest_date.date.year)
 
 # Now add custom forms to populate these data:
 class VarietyForm(ModelForm):
-  class Meta:
-    model = Variety
-    # exclude any ForeignKey or ManyToMany fields
-    exclude = ('diseases',)
-    
+	class Meta:
+		model = Variety
+		# exclude any ForeignKey or ManyToMany fields
+		exclude = ('diseases',)
+		
 class Trial_EntryForm(ModelForm):
-  class Meta:
-    model = Trial_Entry
+	class Meta:
+		model = Trial_Entry
--- wheat_website/wheat_data/views.py
+++ wheat_website/wheat_data/views.py
@@ -18,7 +18,7 @@ def select_location(request):
 			lon2_list = []
 			locations = []
 			today = datetime.date.today()
-			year_list = [today.year,today.year-1,today.year-2,today.year-3] # maybe set this to date.current.year - 3?
+			year_list = [today.year,today.year-1,today.year-2,today.year-3]
 			try:
 				lat1 = float(zipcode.get().latitude) # should only be one result
 				lon1 = float(zipcode.get().longitude) # alternatively, we can call zipcode[0].longitude, but this might throw an IndexError
@@ -27,6 +27,7 @@ def select_location(request):
 				R = 6378137.0 # Earth's median radius, in meters
 				d = radius * 1609.344	 # in meters 
 				# TODO: Search the max distance, then have the user decide what threshold to filter at after _all_ results returned.
+				# TODO: have the Location objects grab default lat/long, not user entered
 				bearing_list = [ 0.0, pi/2.0, pi, 3.0*pi/2.0 ] # cardinal directions
 				for theta in bearing_list:
 					lat2 = asin(sin(lat1)*cos(d/R) + cos(lat1)*sin(d/R)*cos(theta))
@@ -36,7 +37,8 @@ def select_location(request):
 					lon2 = (lon2+3.0*pi)%(2.0*pi) - pi	# normalise to -180...+180
 				lat2_list = lat2_list[0::2] # discard non-moved points
 				lon2_list = lon2_list[1::2] # both should contain two values, {min, max} lat/long
-				# locations = models.Location.objects.filter( # TODO: have the Location objects grab default lat/long
+				
+				"""
 				locations = models.Location.objects.filter(
 						zipcode__latitude__gte=str(lat2_list[1])
 					).exclude(
@@ -46,7 +48,15 @@ def select_location(request):
 					).exclude(
 						zipcode__longitude__gte=str(lon2_list[0])
 					) # doesn't work 100% due to +/- of lat,long numbers...
-				#We just searched a square, now discard searches that are > x miles away.
+				"""
+				
+				locations = models.Location.objects.filter(
+						zipcode__latitude__range=(str(min(lat2_list)), str(max(lat2_list)))
+					).filter(
+						zipcode__longitude__range=(str(min(lon2_list)), str(max(lon2_list)))
+					)
+				#TODO: We just searched a square, now discard searches that are > x miles away.
+			
 			except models.Zipcode.DoesNotExist:
 				return render_to_response(
 					'select_location.html', 
@@ -56,7 +66,8 @@ def select_location(request):
 					},
 					context_instance=RequestContext(request)
 				)
-				
+			
+			"""	
 			entries = [] # this will end up being a 2-dimensional array
 			# TODO: Do not make n=4 queries...
 			for i in year_list: 
@@ -66,27 +77,76 @@ def select_location(request):
 					harvest_date__in=models.Date.objects.filter(date__year=i)
 				 ))
 			
+			#Sort the entries 
+			entries_dict = {} # a dictionary; keys are the variety names, 
+			# value is a 3-item list: [count, list of averages, list of Trial_Entry objects]
+			for entries_by_year in entries:
+				for entry in entries_by_year:
+					if entry.variety.name in entries_dict:
+						entries_dict[entry.variety.name][0] += 1
+						entries_dict[entry.variety.name][2].append(entry)
+					else:
+						entries_dict[entry.variety.name] = [0,[],[entry]]
+			"""
+			
+			""" We cannot use __gte, __lte, etc. queries on date__* fields, a bug since 2009: http://code.djangoproject.com/ticket/6439
+			entries = models.Trial_Entry.objects.select_related(depth=2).filter(
+				location=locations
+			).filter(
+				harvest_date__in=models.Date.objects.filter(
+					date__year__gte=min(year_list)
+				).exclude(
+					date__year__gte=max(year_list)
+				)
+			)
+			"""
+			
+			"""
+			entries = models.Trial_Entry.objects.select_related(depth=2).filter(
+				location=locations
+			).filter(
+				# WARNING: This changes per database implementation...
+				harvest_date__in=models.Date.objects.extra(where=['year IN '+str(tuple(year_list))])
+			)
+			"""
+			
+			entries = models.Trial_Entry.objects.select_related(depth=2).filter(
+				location=locations
+			).filter(
+				harvest_date__in=models.Date.objects.filter(
+					date__range=(datetime.date(min(year_list),1,1), datetime.date(max(year_list),12,31))
+				)
+			)
+			
 			#TODO: At this point, we may want to consider forking the above 
 			# code into a script-enabled version, and have the client sort the
 			# data.
 			
-			#Sort the entries 
-			entries_dict = {} # a dictionary; keys are the variety names, value is a list of Trial_Entry objects
-			for entries_by_year in entries:
-				for entry in entries_by_year:
-					if entry.variety.name in entries_dict:
-						entries_dict[entry.variety.name].append(entry)
-					else:
-						entries_dict[entry.variety.name] = [entry]
+			entries_dict = {} # a dictionary; keys are the variety names, 
+			# values are a 3-item list: 
+			# [count, {dict of fieldname: averages}, [list of Trial_Entry objects]]
+			for entry in entries:
+				if entry.variety.name in entries_dict:
+					entries_dict[entry.variety.name][0] += 1
+					entries_dict[entry.variety.name][2].append(entry)
+				else:
+					entries_dict[entry.variety.name] = [1,{},[entry]]
 			
-			
-			sorted_entries = [] # a list of tuples, (TrialEntry, sum of all data)
-			#for name in entries_dict.keys():
-				#sum = 0.0
-				#for entry in entries_dict[name]:
-					#sum += 1.0
-				#sorted_entries.append((entries_dict[name],))
-				
+			for field in models.Trial_Entry._meta.fields:
+				if (field.get_internal_type() == 'DecimalField' 
+						or field.get_internal_type() == 'PositiveIntegerField' 
+						or field.get_internal_type() == 'SmallIntegerField'
+						or field.get_internal_type() == 'IntegerField'):
+					for key in entries_dict.keys():
+						entries_dict[key][1][field.name] = 0.0 # will be a float even if the field is an integer
+						for entry_value in entries_dict[key][2]:
+							#print(entry_value._meta.get_field(field.name))
+							#print(field.get_internal_type())
+							#print(getattr(entry_value, field.name))						
+							if getattr(entry_value, field.name) != None:
+								entries_dict[key][1][field.name] += float(getattr(entry_value, field.name))
+						entries_dict[key][1][field.name] /= float(entries_dict[key][0])
+
 			#TODO: Use HttpResponseRedirect(), somehow passing the variables, so that the user can use the back-button
 			# hmm... the back-button works, but it's not obvious it will based on the address bar
 			# I'd still like this all to use the address bar to pass the user input
@@ -98,7 +158,7 @@ def select_location(request):
 				{ 
 					'location_list': locations,
 					'trialentry_list': entries,
-					'trialentry_dict': sorted_entries,
+					'trialentry_dict': entries_dict,
 					'year_list': year_list,
 					'lat_list': lat2_list,
 					'lon_list': lon2_list
