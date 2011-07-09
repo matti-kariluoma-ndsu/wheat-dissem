527511bf41d73b5ce870ed77b4c6c7d7f92f8ad7
Matti Kariluoma <matti.kariluoma@gmail.com>
2011-07-09 12:44:26 -0500

    Beginning more work on the CVS upload poriton before going back to Data presentation, esp. LSD etc. -MMK Jul 2011

--- wheat_website/wheat_data/wheat_forms.py
+++ wheat_website/wheat_data/wheat_forms.py
@@ -17,7 +17,7 @@ class SelectVarietyForm(forms.Form):
 	variety = forms.CharField(max_length=20)
 
 class UploadCSVForm(forms.Form):
-    csv_file  = forms.FileField()
+	csv_file  = forms.FileField()
 
 def handle_csv_file(uploaded_file):
 	#reader = csv.reader(open(uploaded_file), dialect='excel')
@@ -29,7 +29,8 @@ def handle_csv_file(uploaded_file):
 		print("!!!"+row+"!!!")
 	"""
 
-	insertion_list = []
+	insertion_dict = {}
+	reference_dict = {}
 
 	skip = True
 	skip_lines = 1
@@ -38,41 +39,46 @@ def handle_csv_file(uploaded_file):
 	# Inspect our model, to grab the fields from it
 
 	for field in models.Trial_Entry._meta.fields:
-		if (skip):
-			if (skip_count+1 >= skip_lines):
-				skip = False
-			else:
-				skip_count += 1
+		if (field.get_internal_type() == 'ForeignKey' 
+				or field.get_internal_type() == 'ManyToManyField' ):
+			reference_dict[field.name] = field.rel.to.objects.all()
 		else:
-			if (field.get_internal_type() == 'ForeignKey' 
-					or field.get_internal_type() == 'ManyToManyField' ):
-				#print ("Skipping reference field")
-				pass
-			else:
-				#print(field.name)
-				insertion_list.append([field.name, None])
-
-	#Now inspect the uploaded file and attempt to write it all to database
+			insertion_dict[field.name] = None
+				
+	#print reference_dict['variety'][0].id
+	
+	# Now inspect the uploaded file and attempt to write it all to database
 	# TODO: Consider not save() -ing each line, maybe do batches of ~100?
 	skip = True
 	skip_lines = 2
-	skip_count = 0
+	line_number = 0
+	headers = []
+	errors = []
 	
 	for line in uploaded_file:
+		line_number += 1
 		if (skip):
-			if (skip_count+1 >= skip_lines):
+			if (line_number + 1 > skip_lines):
+				headers = str(line).replace('"','').split(',') # assume the headers are the second row
 				skip = False
-			else:
-				skip_count += 1
 		else:
-			#print("!!!"+line+"!!!")
-			insert_pos = 0
+			column_number = 0
 			for column in str(line).split(','):
-				insertion_list[insert_pos][1] = column
-				insert_pos += 1
+				try:
+					name = headers[column_number].strip()
+					if name in insertion_dict.keys():
+						insertion_dict[headers[column_number]] = column
+					else:
+						if name in reference_dict.keys():
+							pass
+						else:
+							errors.append("Heading name \"%s\" not found in database." % name)
+				except IndexError:
+					errors.append("Found more data columns than there are headings. Line: %d, Column: %d" % (line_number, column_number))
+				column_number += 1
 			model_instance = models.Trial_Entry()
-			for name_value_list in insertion_list:
-				model_instance.__setattr__(name_value_list[0],name_value_list[1])
+			for name in insertion_dict.keys():
+				setattr(model_instance, name, insertion_dict[name])
 			#model_instance.save() # ARE YOU BRAVE ENOUGH?
 			
 			
