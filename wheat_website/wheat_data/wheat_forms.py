7d1f9952738ffa7964cba2a0d7234fd3e4e24f2a
Matti Kariluoma <matti.kariluoma@gmail.com>
2011-07-09 17:25:18 -0500

    More work on the CSV uploader, now with fuzzy spellchecker!
    -Matti Kariluoma Jul 2011

--- wheat_website/wheat_data/wheat_forms.py
+++ wheat_website/wheat_data/wheat_forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from wheat_data import models
+from difflib import SequenceMatcher
 #import csv
 
 class SelectLocationForm(forms.Form):
@@ -18,6 +20,94 @@ class SelectVarietyForm(forms.Form):
 
 class UploadCSVForm(forms.Form):
 	csv_file  = forms.FileField()
+
+class fuzzy_spellchecker():
+	""" Uses an internal dictionary to check whether a word has a close 
+	match or not.	"""
+	_words = {}
+	
+	def __init__(self):
+		pass
+		
+	def __init__(self, word_dict):
+		self.set_dictionary(word_dict)
+		
+	def set_dictionary(self, word_dict):
+		self._words = word_dict
+		
+	def check(self, field, word):
+		"""
+		Returns either the closest matched word or None in the specified field
+		"""
+		try:
+			self._words[field]
+		except KeyError:
+			return None
+		
+		corrected = None
+		ratios = {}
+		
+		for entry in self._words[field]:
+			name = str(entry)
+			if isinstance(entry, models.Date):
+				date = str(forms.DateField().clean(word))# raises a ValidationError
+				if  date == name: 
+					corrected = name
+					break
+				#else:
+					#m = SequenceMatcher(None, date, name)
+					#ratios[name] = m.ratio()
+					#print "%s ? %s: %f" % (word, name, m.ratio())
+			else:
+				if word == name:
+					corrected = name
+					break
+				elif word.lower() == name.lower():
+					corrected = name
+					break					
+				else:
+					m = SequenceMatcher(None, word, name)
+					ratios[name] = m.ratio()
+					#print "%s ? %s: %f" % (word, name, m.ratio())
+				
+		if corrected is None:
+			smax = 0.0
+			for key in ratios.keys():
+				if ratios[key] > smax:
+					smax = ratios[key]
+					corrected = key
+		
+		return corrected
+
+def handle_reference_field(reference_dict, field, data):
+	"""
+	Given a spellchecker, a dictionary of possible reference values, a 
+	requested field to populate, and thedata to populate with, find/create
+	the field and returns it's id to populate the reference field with.
+	"""
+	try:
+		reference_dict[field]
+	except KeyError:
+		return None
+	
+	speller = fuzzy_spellchecker(reference_dict)
+	word = speller.check(field, data)
+	return_id = None
+	
+	if word is None: # create a new object and save it to the db
+		print "make new %s called %s" % (field, data)
+		try:
+			if isinstance(reference_dict[field][0], models.Date):
+		except IndexError:
+			pass
+	else: # lookup the existing object 
+		for entry in reference_dict[field]: # if field isn't None, then it's a key
+			if word == str(entry):
+				return_id = entry.id
+				print "found a match for %s: %s, %d" % (data, word, return_id)
+				break
+		
+	return return_id
 
 def handle_csv_file(uploaded_file):
 	#reader = csv.reader(open(uploaded_file), dialect='excel')
@@ -41,7 +131,7 @@ def handle_csv_file(uploaded_file):
 	for field in models.Trial_Entry._meta.fields:
 		if (field.get_internal_type() == 'ForeignKey' 
 				or field.get_internal_type() == 'ManyToManyField' ):
-			reference_dict[field.name] = field.rel.to.objects.all()
+			reference_dict["%s_id" % (field.name)] = field.rel.to.objects.all()
 		else:
 			insertion_dict[field.name] = None
 				
@@ -55,6 +145,7 @@ def handle_csv_file(uploaded_file):
 	headers = []
 	errors = []
 	
+	
 	for line in uploaded_file:
 		line_number += 1
 		if (skip):
@@ -63,23 +154,26 @@ def handle_csv_file(uploaded_file):
 				skip = False
 		else:
 			column_number = 0
-			for column in str(line).split(','):
+			for column in str(line).replace('"','').split(','):
 				try:
 					name = headers[column_number].strip()
 					if name in insertion_dict.keys():
-						insertion_dict[headers[column_number]] = column
+						insertion_dict[name] = column.strip()
 					else:
 						if name in reference_dict.keys():
-							pass
+							try:
+								insertion_dict[name] = handle_reference_field(reference_dict, name, column.strip())
+							except ValidationError:
+								errors.append("Couldn't read a badly formatted date on Row: %d, Column: %d: \"%s\"" % (line_number, column_number, column.strip()))
 						else:
 							errors.append("Heading name \"%s\" not found in database." % name)
 				except IndexError:
-					errors.append("Found more data columns than there are headings. Line: %d, Column: %d" % (line_number, column_number))
+					errors.append("Found more data columns than there are headings. Row: %d, Column: %d" % (line_number, column_number))
 				column_number += 1
 			model_instance = models.Trial_Entry()
 			for name in insertion_dict.keys():
 				setattr(model_instance, name, insertion_dict[name])
 			#model_instance.save() # ARE YOU BRAVE ENOUGH?
 			
-			
+	print errors
 	return False, {'file': 'not in csv format', 'variety': 'not an id'}
