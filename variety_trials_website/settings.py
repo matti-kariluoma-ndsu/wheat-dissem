6d781441dc169c165d5221827e607aa16f5dccaa
Variety Selection <varieties@ndsuext.nodak.edu>
2012-04-13 12:09:09 -0500

    Inserted the data into postgresql.

--- variety_trials_website/settings.py
+++ variety_trials_website/settings.py
@@ -13,10 +13,10 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'varieties',                      # Or path to database file if using sqlite3.
+        'USER': 'varieties',                      # Not used with sqlite3.
+        'PASSWORD': 'tyarbEc2',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
