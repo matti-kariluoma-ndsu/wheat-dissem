b3d0b52a1ee4bb9bc393a2a11db485f846731fb8
Matti Kariluoma <matti.kariluoma@gmail.com>
2012-09-14 15:26:05 -0500

    same error, two places

--- variety_trials_data/views.py
+++ variety_trials_data/views.py
@@ -324,7 +324,7 @@ def zipcode_view(request, year_range, fieldname, abtest=None):
 			try:
 				locations = get_locations(zipcode, scope)
 			except models.Zipcode.DoesNotExist:
-				zipcode_radius_form = variety_trials_forms.SelectLocationByZipcodeRadiusForm(initial={
+				zipcode_radius_form = variety_trials_forms.SelectLocationByZipcodeForm(initial={
 						'scope': scope,
 					})
 				# TODO: return to main page and show error
