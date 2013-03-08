6eb98e512c717d57a79c02211570f453f4ed6070
Matti Kariluoma <matti@kariluo.ma>
2013-03-07 19:09:03 -0600

    delete all existing code, replace with fresh implementation

--- variety_trials_data/page/table/Column.py
+++ variety_trials_data/page/table/Column.py
@@ -2,11 +2,19 @@ class Column:
 	"""
 	Contains references to each Cell in this column.
 	"""
-	def __init__(self, location):
+	def __init__(self, location, page):
+		self.clear()
 		self.location = location
-		self.members = []
-		self.index = 0
+		self.page = page
 	
+	def __unicode__(self):
+		column = [unicode(self.location)]
+		column.extend([unicode(cell) for cell in self])
+		return unicode(" ").join(column)
+		
+	def __str__(self):
+		return str(unicode(self))
+		
 	def __iter__(self):
 		self.index = 0
 		return self
@@ -22,41 +30,32 @@ class Column:
 		self.members.append(value)
 		
 	def clear(self):
-		for m in self.members:
-			if isinstance(m, Cell):
-				m.delete_column()
 		self.members = []
 		self.index = 0
-		
-	def __unicode__(self):
-		column = [unicode(self.location)]
-		column.extend([unicode(cell) for cell in self])
-		return unicode(" ").join(column)
-
-class Aggregate_Column(Column):
-	"""
-	A column whose cells' value is determined by other cells in its row
-	"""
-	def __init__(self, location, year_num):
-		"""
-		location: a Location (or Fake_Location) object
-		year_num: an integer denoting the number of years to go back for averaging i.e. 3
-		"""
-		Column.__init__(self, location)
-		self.members = []
-		self.clear()
-		self.years_range = range(year_num)
-		
-	def get_site_years(self):
-		return self.site_years
-		
-	def clear(self):
-		Column.clear(self)
-		self.site_years = None
-		self.years_range = []
+		self.site_years = 8
+		self.location = None
+		self.page = None
 
 class Fake_Location:
 	def __init__(self, name):
 		self.name = name
 		self.id = -1
 		self.pk = -1
+
+class Aggregate_Column(Column):
+	"""
+	A column whose cells' value is determined by other cells in its row
+	"""
+	def __init__(self, location, years_back):
+		"""
+		location: a Location (or Fake_Location) object
+		year_num: an integer denoting the number of years to go back for averaging i.e. 3
+		"""
+		Column.__init__(self, location)
+		self.years_back = years_back
+		
+	def clear(self):
+		Column.clear(self)
+		years_back = 1
+
+
