ee95d939652b3b00ba8b75557e1368d0d060eb7e
Matti Kariluoma <matti@kariluo.ma>
2013-03-24 14:38:15 -0500

    removed temp file, has been applied to all databases. made make_page execute from shell

--- make_page.py
+++ make_page.py
@@ -1,1 +1,1 @@
-from variety_trials_data.variety_trials_util import get_page; p = get_page("58102", "NEAR", 2012, "bushels_acre", ""); print p
+import os; os.environ.setdefault("DJANGO_SETTINGS_MODULE", "variety_trials_website.settings"); from variety_trials_data.variety_trials_util import get_page; p = get_page("58102", "NEAR", 2012, "bushels_acre", ""); print p
