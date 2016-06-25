6ade3b98a9e3a56f9d02ce59edd0e7bda59424c5
Matti Kariluoma <matti@kariluo.ma>
2016-06-25 11:30:54 -0500

    expunge old add interface

--- variety_trials_website/urls.py
+++ variety_trials_website/urls.py
@@ -49,16 +49,6 @@ urlpatterns = patterns('',
 		(r'^data/variety/all/json/', json_views.variety_json_all),
 		(r'^data/variety/id_(?P<id>[0-9]+)/json/', json_views.variety_json),
 		(r'^data/disease/id_(?P<id>[0-9]+)/json/', json_views.disease_json),
-		
-		(r'^add/$', views.howto_add_data),
-		(r'^add/trial_entries/$', adding_data_views.add_trial_entry_csv_file), # page to upload a spreadsheet to
-		(r'^add/trial_entries/confirm/', adding_data_views.add_trial_entry_csv_file_confirm), # page to upload a spreadsheet to
-		(r'^add/trial_entry/', adding_data_views.add_trial_entry),
-		(r'^add/date/', adding_data_views.add_date),
-		(r'^add/location/', adding_data_views.add_location),
-		(r'^add/variety/', adding_data_views.add_variety), 
-		(r'^add/history/', adding_data_views.history), # defaults to location-based view
-		(r'^add/history/delete/(?P<delete>[0-9]+)/', adding_data_views.history_delete), # defaults to location-based view
-		
+				
 )
 
